990220c45af638fee2aad3ba2ff0ca33c301b996
Khozema Nullwala <khozema.nullwala@accenture.com>
2019-06-24 19:48:28 +0530

    Finalize the changes after checking the Sonar Analysis Report!

--- src/main/java/com/accenture/lkm/controller/VendorController.java
+++ src/main/java/com/accenture/lkm/controller/VendorController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.lkm.business.bean.VendorBean;
@@ -19,7 +17,7 @@ public class VendorController {
 	@Autowired
 	private VendorService vendorService;
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@GetMapping("/")
 	public String index() {
 		return "Welcome to Vendor Service API!";
 	}
